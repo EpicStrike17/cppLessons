100f1103aaf6db51d322b71afa316a43c593d369
Polina <sysoeva_polina@mail.ru>
2022-03-06 21:47:29 +0300

    getdamage

--- Polina/Proj1/Polina/Polina.cpp
+++ Polina/Proj1/Polina/Polina.cpp
@@ -25,37 +25,74 @@ public:
 
     bool islife() {
         if (hp > 0) {
-            cout << "i am alive" << endl;
             return true;
-
         }
+        cout << "i am dead" << endl;
+        return false;
+    }
+
+    void Kick(Hero* enemy) {
+        enemy->getDamage(attack);
+        if (!islife()) return;
     }
 
 
+    void getDamage(int dmg) {
+        hp -= dmg;
+        if (hp <= 0) {
+            cout << "i am dead" << endl;
+
+        }
+}
 };
 
 
-Hero createh(string pass) {
+Hero* createh(string pass) {
     ifstream file(pass, fstream::in);
     
-    Hero newplayer;
+    Hero* newplayer = new Hero();
 
 
-    file >> newplayer.name >> newplayer.hp >> newplayer.attack;
-    cout << "\n name:" << newplayer.name;
-    cout << "\n hp:" << newplayer.hp;
-    cout << "\n attack:" << newplayer.attack;
+    file >> newplayer->name >> newplayer->hp >> newplayer->attack;
+    cout << "\n name:" << newplayer->name;
+    cout << "\n hp:" << newplayer->hp;
+    cout << "\n attack:" << newplayer->attack;
     file.close();
     return newplayer;
 }
 
+string Menu(string cmd) {
+    string newCmd = "";
+    if (cmd == "main") {
+        cout << endl << "1. Select hero";
+        cout << endl << "x. Exit";
+        
+        cin >> newCmd;
+        if (newCmd == "1") return "selectHero";
+        if (newCmd == "x") return "exit";
+
+    }
+    if (cmd == "selectHero") {
+        cout << endl << "1. Player1";
+        cout << endl << "2. Player2" << endl;
+        cin >> newCmd;
+
+
+    }
+
+}
+
+
 int main()
 {
-    createh("player1.txt");
-    createh("player2.txt");
+   Hero* player1 = createh("player1.txt");
+   Hero* player2 = createh("player2.txt");
 
+    player1->Kick(player2);
+   
+   
 
-    Hero player1;
+   
 
 
     
