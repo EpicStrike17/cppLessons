3413518553983d343fddc55022bdd6bfb0f1afd5
Vladimir Kozlov <vladimir.kozlov@mytona.com>
2022-03-16 19:01:12 +0300

    add Yura laba2

---
+++ Yura/Laba2/Laba2/Laba2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int sum = 0;
+	int arr[10];
+	srand(time(0));
+
+	int N;
+	cout << "Type N: ";
+	cin >> N;
+
+	cout<< "\nArray: ";
+	bool sign = true;
+	
+	for (int i = 0; i < N; i++) {
+		*(arr + i) = rand() % 100;
+		cout << *(arr + i) << " ";
+
+		sum += (sign ? 1 : -1) * *(arr + i);
+		sign = !sign;
+	};
+
+
+	cout<< "\nSum: "<< sum<< endl;
+
+	cout << endl;
+	system("pause");
+}
